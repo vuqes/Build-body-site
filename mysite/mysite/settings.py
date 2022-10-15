e887fee857a0d268e4ba96287f2b7404acfeb9c6
vuqes <maybug17@mail.ru>
2022-10-15 17:36:51 +0300

    новый файл:    mysite/.idea/vcs.xml 	изменено:      mysite/mysite/__pycache__/settings.cpython-310.pyc 	изменено:      mysite/mysite/settings.py 	изменено:      mysite/vuqes/__pycache__/views.cpython-310.pyc

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -151,7 +151,7 @@ EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES' : [
+    'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
