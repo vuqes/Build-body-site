4154f4c7bf95278a126376a8658e5732b208309e
vuqes <maybug17@mail.ru>
2022-10-29 18:11:05 +0300

    изменено:      db.sqlite3 	изменено:      mysite/__pycache__/urls.cpython-310.pyc 	изменено:      vuqes/__pycache__/urls.cpython-310.pyc 	изменено:      vuqes/urls.py

--- mysite/vuqes/urls.py
+++ mysite/vuqes/urls.py
@@ -3,13 +3,13 @@ from .views import *
 
 urlpatterns = [
     path('', VuqesHome.as_view(), name='home'),
-    path('<slug:slug>/', index, name='index'),
-    path('/write/', write_up, name='write'),
-    path('/register/', RegisterUser.as_view(), name='register'),
-    path('/login/', LoginUser.as_view(), name='login'),
-    path('/logout/', logout_user, name='logout'),
-    path('/contact/', ContactFormView.as_view(), name='contact'),
-    path('/cat/', category, name='cat'),
-    path('/about/', AboutUs.as_view(), name='about'),
-    path('/API/', about_API, name='API')
+    path('post/<slug:slug>/', index, name='index'),
+    path('write/', write_up, name='write'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('cat/', category, name='cat'),
+    path('about/', AboutUs.as_view(), name='about'),
+    path('API/', about_API, name='API')
 ]
